2438f139dda9c240e0764b46acca36d4a29d454c
Rajwara <maliksaadsultan766@gmail.com>
2024-03-12 22:22:27 +0500

    dummy data of invetory screen added

---
+++ src/data/inventery-data.ts
@@ -0,0 +1,241 @@
+export type InventeryType = {
+    id: string;
+    itemname: string;
+    vendor: string;
+    unitprice: number;
+    unitofMeasure: number;
+    onhandqty: number;
+    incomingqty: number;
+    reservedqty:number;
+  };
+  
+  export const inventeryData = [
+    {
+      id: '0o02051402',
+      itemname: 'Tasty Metal Shirt',
+      vendor: 'Books',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/7.webp',
+      unitprice: '410.00',
+      unitofMeasure: '410.00',
+      onhandqty: "99",
+      incomingqty:"7380",
+      reservedqty:"23",
+    },
+    {
+      id: '0o17477064',
+      itemname: 'Modern Cotton Gloves',
+      vendor: 'Kids',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/3.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+        onhandqty: "99",
+        incomingqty:"7380",
+        reservedqty:"23",
+     
+    },
+    {
+      id: '0o02374305',
+      itemname: 'Rustic Steel Computer',
+      vendor: 'Games',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/1.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+        onhandqty: "99",
+        incomingqty:"7380",
+        reservedqty:"23",
+     
+    },
+    {
+      id: '0o02602714',
+      itemname: 'Licensed Concrete Cheese',
+      vendor: 'Electronics',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/11.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+        onhandqty: "99",
+        incomingqty:"7380",
+        reservedqty:"23",
+    
+    },
+    {
+      id: '0o54011366',
+      itemname: 'Electronic Rubber Table',
+      vendor: 'Books',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/15.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+        onhandqty: "99",
+        incomingqty:"7380",
+        reservedqty:"23",
+     
+    },
+    {
+      id: '0o24033230',
+      itemname: 'Gorgeous Bronze Gloves',
+      vendor: 'Shoes',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/16.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+        onhandqty: "99",
+        incomingqty:"7380",
+        reservedqty:"23",
+    },
+    {
+      id: '0o27342230',
+      itemname: 'Practical Steel Keyboard',
+      vendor: 'Kids',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/6.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+        onhandqty: "99",
+        incomingqty:"7380",
+        reservedqty:"23",
+    },
+    {
+      id: '0o64235224',
+      itemname: 'Sleek Frozen Ball',
+      vendor: 'Electronics',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/8.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+        onhandqty: "99",
+        incomingqty:"7380",
+        reservedqty:"23",
+    },
+    {
+      id: '0o63671734',
+      itemname: 'Ergonomic Frozen Pants',
+      vendor: 'Games',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/9.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+        onhandqty: "99",
+        incomingqty:"7380",
+        reservedqty:"23",
+     
+    },
+    {
+      id: '0o60206537',
+      itemname: 'Sleek Fresh Chair',
+      vendor: 'Garden',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/10.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+        onhandqty: "99",
+        incomingqty:"7380",
+        reservedqty:"23",
+    },
+    {
+      id: '0o53505174',
+      itemname: 'Awesome Granite Chicken',
+      vendor: 'Electronics',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/12.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+        onhandqty: "99",
+        incomingqty:"7380",
+        reservedqty:"23",
+    },
+    {
+      id: '0o20360446',
+      itemname: 'Rustic Concrete Ball',
+      vendor: 'Books',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/4.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+        onhandqty: "99",
+        incomingqty:"7380",
+        reservedqty:"23",
+   
+    },
+    {
+      id: '0o05416424',
+      itemname: 'Electronic Concrete Computer',
+      vendor: 'Tools',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/13.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+        onhandqty: "99",
+        incomingqty:"7380",
+        reservedqty:"23",
+    },
+    {
+      id: '0o52110435',
+      itemname: 'Small Wooden Pizza',
+      vendor: 'Toys',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/14.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+        onhandqty: "99",
+        incomingqty:"7380",
+        reservedqty:"23",
+    },
+    {
+      id: '0o40214300',
+      itemname: 'Tasty Bronze Salad',
+      vendor: 'Baby',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/4.webp',
+        unitprice: '410.00',
+      unitofMeasure: '410.00',
+      onhandqty: "99",
+      incomingqty:"7380",
+      reservedqty:"23",
+    },
+    {
+      id: '0o02051402',
+      itemname: 'Tasty Metal Shirt',
+      vendor: 'Books',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/7.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+     onhandqty: "99",
+     incomingqty:"7380",
+     reservedqty:"23",
+    },
+    {
+      id: '0o17477064',
+      itemname: 'Modern Cotton Gloves',
+      vendor: 'Kids',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/3.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+        onhandqty: "99",
+        incomingqty:"7380",
+        reservedqty:"23",
+    },
+    {
+      id: '0o02374305',
+      itemname: 'Rustic Steel Computer',
+      vendor: 'Games',
+      image:
+        'https://isomorphic-furyroad.s3.amazonaws.com/public/products/modern/1.webp',
+        unitprice: '410.00',
+        unitofMeasure: '410.00',
+        onhandqty: "99",
+        incomingqty:"7380",
+        reservedqty:"23",
+    },
+  ];
+  
+  
+  
+  
+  
+  
