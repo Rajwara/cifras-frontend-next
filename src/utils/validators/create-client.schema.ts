f90890094ee7d418c310f68236a115bce9bd36e3
Rajwara <maliksaadsultan766@gmail.com>
2024-03-22 21:40:55 +0500

    schema changed for field vales

--- src/utils/validators/create-client.schema.ts
+++ src/utils/validators/create-client.schema.ts
@@ -4,12 +4,21 @@ import { fileSchema } from '@/utils/validators/common-rules';
 export const clientFormSchema = z.object({
   name: z.string().min(1, { message: messages.nameRequired }),
   description: z.string().optional(),
-  phoneNumber: z.coerce.number().min(1, { message: messages.phoneNumberRequired }),
-  alias: z.string().min(1, { message: messages.aliasRequired }),
+  phone: z.coerce.string().min(1, { message: messages.phoneNumberRequired }),
+  alias: z.string().optional(),
   images: z.array(fileSchema).optional(),
   email: z.string().min(1, { message: messages.emailRequired }),
   ruc: z.string().min(1, { message: messages.rucRequired }),
   dv: z.string().min(1, { message: messages.dvRequired }),
+  addressType: z.string().min(1, { message: messages.addressType }),
+  firstStreet: z.string().min(1, { message: messages.firstStreet }),
+  secondStreet: z.string().min(1, { message: messages.secondStreet }),
+  province: z.string().min(1, { message: messages.province }),
+  district: z.string().min(1, { message: messages.district }),
+  jurisdiction: z.string().min(1, { message: messages.jurisdiction }),
+  country: z.string().min(1, { message: messages.country }),
+  receptorFeType: z.string().min(1, { message: messages.receptorFeType }),
+  contributorType: z.string().min(1, { message: messages.contributorType }),
 
 });
 export type CreateClientInput = z.infer<typeof clientFormSchema>;
